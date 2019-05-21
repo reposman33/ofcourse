a3d6c158e41a1247150053ed640564ccd5605dca
Marc <marc.webdev@yahoo.com>
2019-05-21 13:55:34 +0200

    move handleError method from actions to api mehods

--- src/actions.js
+++ src/actions.js
@@ -5,7 +5,9 @@ import {
 	createLesson,
 	getLessons,
 	saveLesson,
-	deleteLesson
+	deleteLesson,
+	loginUser,
+	createUser
 } from "./api";
 const REMOVE_COURSE = "REMOVE_COURSE";
 const ADD_COURSE_BEGIN = "ADD_COURSE_BEGIN";
@@ -34,83 +36,95 @@ const SAVE_LESSON_ERROR = "SAVE_LESSON_ERROR";
 const SET_LESSONMARKDOWN = "SET_LESSONMARKDOWN";
 const SET_LESSONMARKDOWN_ERROR = "SET_LESSONMARKDOWN_ERROR";
 const TOGGLE_PREVIEW_MODE = "TOGGLE_PREVIEW_MODE";
+const LOGIN_BEGIN = "LOGIN_BEGIN";
+const LOGIN_SUCCESS = "LOGIN_SUCCESS";
+const LOGIN_ERROR = "LOGIN_ERROR";
+const SIGNUP_BEGIN = "SIGNUP_BEGIN";
+const SIGNUP_SUCCESS = "SIGNUP_SUCCESS";
+const SIGNUP_ERROR = "SIGNUP_ERROR";
 
 const openNewCourseModal = () => ({ type: OPEN_NEW_COURSE_MODAL });
 const closeNewCourseModal = () => ({ type: CLOSE_NEW_COURSE_MODAL });
 
-const verifyResponse = res => {
-	if (!res.ok) {
-		throw res;
-	} else {
-		return res.json();
-	}
-};
-
 const addCourse = ({ name, price }) => (dispatch, getState) => {
 	dispatch({ type: ADD_COURSE_BEGIN });
 	createCourse({ name, price })
-		.then(res => verifyResponse(res))
 		.then(courseData => {
 			dispatch({ type: ADD_COURSE_SUCCESS, payload: courseData });
 			dispatch({ type: CLOSE_NEW_COURSE_MODAL });
 		})
-		.catch(error => dispatch({ type: ADD_COURSE_ERROR, error }));
+		.catch(error => dispatch({ type: ADD_COURSE_ERROR, payload: error }));
 };
 
 const removeCourse = course => (dispatch, getState) => {
 	dispatch({ type: REMOVE_COURSE_BEGIN });
 	deleteCourse(course)
-		.then(res => verifyResponse(res))
 		.then(res => dispatch({ type: REMOVE_COURSE_SUCCESS }))
-		.catch(error => dispatch({ type: REMOVE_COURSE_ERROR, error }));
+		.catch(error =>
+			dispatch({ type: REMOVE_COURSE_ERROR, payload: error })
+		);
 };
 
 const loadCourses = () => (dispatch, getState) => {
 	dispatch({ type: GET_COURSES_BEGIN });
 	getCourses()
-		.then(res => verifyResponse(res))
 		.then(courseData =>
 			dispatch({ type: GET_COURSES_SUCCESS, payload: courseData })
 		)
-		.catch(error => dispatch({ type: GET_COURSES_ERROR, error }));
+		.catch(error => dispatch({ type: GET_COURSES_ERROR, payload: error }));
 };
 
 const updateLesson = lesson => (dispatch, getState) => {
 	dispatch({ type: SAVE_LESSON_BEGIN });
 	saveLesson(lesson)
-		.then(res => verifyResponse(res))
 		.then(lesson =>
 			dispatch({ type: SAVE_LESSON_SUCCESS, payload: lesson })
 		)
-		.catch(error => dispatch({ type: SAVE_LESSON_ERROR, error }));
+		.catch(error => dispatch({ type: SAVE_LESSON_ERROR, payload: error }));
 };
 
 const addLesson = lesson => (dispatch, getState) => {
 	dispatch({ type: ADD_LESSON_BEGIN });
 	createLesson(lesson)
-		.then(res => verifyResponse(res))
 		.then(lesson => dispatch({ type: ADD_LESSON_SUCCESS, payload: lesson }))
-		.catch(error => dispatch({ type: ADD_LESSON_ERROR, error }));
+		.catch(error => dispatch({ type: ADD_LESSON_ERROR, payload: error }));
 };
 
 const loadLessons = id => (dispatch, getState) => {
 	dispatch({ type: LOAD_LESSONS_BEGIN });
 	getLessons(id)
-		.then(res => verifyResponse(res))
 		.then(lessons =>
 			dispatch({ type: LOAD_LESSONS_SUCCESS, payload: lessons })
 		)
-		.catch(error => dispatch({ type: LOAD_LESSONS_ERROR, error }));
+		.catch(error => dispatch({ type: LOAD_LESSONS_ERROR, payload: error }));
 };
 
 const removeLesson = lessonId => (dispatch, getState) => {
 	dispatch({ type: REMOVE_LESSON_BEGIN });
 	deleteLesson(lessonId)
-		.then(res => verifyResponse(res))
 		.then(dispatch({ type: REMOVE_LESSON_SUCCESS, payload: lessonId }))
 		.catch(error =>
 			dispatch({ type: REMOVE_LESSON_ERROR, payload: error })
 		);
+};
+
+const onLogin = (username, password) => dispatch => {
+	dispatch({ type: LOGIN_BEGIN });
+	loginUser(username, password)
+		.then(user =>
+			dispatch({
+				type: LOGIN_SUCCESS,
+				payload: user
+			})
+		)
+		.catch(error => dispatch({ type: LOGIN_ERROR, payload: error }));
+};
+
+const onSignUp = (username, password) => dispatch => {
+	dispatch({ type: SIGNUP_BEGIN });
+	createUser(username, password)
+		.then(user => dispatch({ type: SIGNUP_SUCCESS, payload: user }))
+		.catch(error => dispatch({ type: SIGNUP_ERROR, payload: error }));
 };
 
 let timeoutId = null;
@@ -160,6 +174,12 @@ export {
 	SET_LESSONMARKDOWN,
 	SET_LESSONMARKDOWN_ERROR,
 	TOGGLE_PREVIEW_MODE,
+	LOGIN_BEGIN,
+	LOGIN_SUCCESS,
+	LOGIN_ERROR,
+	SIGNUP_BEGIN,
+	SIGNUP_SUCCESS,
+	SIGNUP_ERROR,
 	openNewCourseModal,
 	closeNewCourseModal,
 	addCourse,
@@ -170,5 +190,7 @@ export {
 	updateLesson,
 	removeLesson,
 	setLessonMarkdown,
-	togglepreviewMode
+	togglepreviewMode,
+	onLogin,
+	onSignUp
 };
--- src/api.js
+++ src/api.js
@@ -1,24 +1,38 @@
 const PREFIX = "/api";
 
-const postData = (url, data = {}) => fetchWithData(url, data, "POST");
-
-const putData = (url, data = {}) => fetchWithData(url, data, "PUT");
+const handleError = res => {
+	if (!res.ok) {
+		return res.json().then(body => {
+			throw new Error(body.message);
+		});
+	} else {
+		return res.json();
+	}
+};
 
 const fetchWithData = (url, data = {}, method = "POST") => {
 	return fetch(url, {
 		method: method,
 		headers: { "content-type": "Application/Json" },
 		body: JSON.stringify(data)
-	}).then(response => response);
+	}).then(res => handleError(res));
 };
 
-const getCourses = () => fetch(PREFIX + "/courses");
+const postData = (url, data = {}) => fetchWithData(url, data, "POST");
+
+const putData = (url, data = {}) => fetchWithData(url, data, "PUT");
+
+const deleteData = (url, data = {}) => fetchWithData(url, data, "DELETE");
+
+const getCourses = () =>
+	fetch(PREFIX + "/courses").then(res => handleError(res));
 
 const createCourse = course => postData(PREFIX + "/courses", course);
 
 const deleteCourse = course => deleteData(PREFIX + "/courses", course);
 
-const getLessons = id => fetch(PREFIX + `/lessons?courseId=${id}`);
+const getLessons = id =>
+	fetch(PREFIX + `/lessons?courseId=${id}`).then(res => handleError(res));
 
 const createLesson = lesson => postData(PREFIX + "/lessons", lesson);
 
@@ -26,10 +40,14 @@ const saveLesson = lesson => putData(PREFIX + `/lessons/${lesson.id}`, lesson);
 
 const deleteLesson = lessonId => deleteData(PREFIX + `/lessons/${lessonId}`);
 
-const deleteData = (url, data = {}) => fetchWithData(url, data, "DELETE");
-
 const saveLessonMarkdown = (lesson, markdown) =>
 	putData(PREFIX + `/lessons/${lesson.id}`, { ...lesson, markdown });
+
+const loginUser = (username, password) =>
+	postData(PREFIX + `/login`, { username, password });
+
+const createUser = (username, password) =>
+	putData(PREFIX + `/users`, { username, password });
 
 export {
 	createCourse,
@@ -39,5 +57,7 @@ export {
 	getLessons,
 	saveLesson,
 	deleteLesson,
-	saveLessonMarkdown
+	saveLessonMarkdown,
+	loginUser,
+	createUser
 };
